84481e37ee27c4d2daf497b2f4a70389dd3ec08d
amal laassikri <amal.laassikri@student.kdg.be>
2023-02-22 15:03:07 +0100

    class player added

---
+++ Integration 2 - TAKE 5/Integration 2 - TAKE5/src/Card.java
@@ -0,0 +1,6 @@
+public class Card {
+    private int number;
+    private int amountOfBulls;
+    private int value;
+
+}
---
+++ Integration 2 - TAKE 5/Integration 2 - TAKE5/src/Table.java
@@ -0,0 +1,4 @@
+public class Table {
+    static final int[] ROWS = {1, 2, 3, 4};
+    static final int[] COLUMNS = {1, 2, 3, 4};
+}
