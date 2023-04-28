0d65ad719f5f0c64b53e773ffbd433651e941bc0
Aleksandra <aleksandra.angelova@student.kdg.be>
2023-04-29 01:24:07 +0200

    rule 1 (draft)

--- Integration 2 - TAKE5 - Game/src/be/kdg/integration2/take5/model/AI.java
+++ Integration 2 - TAKE5 - Game/src/be/kdg/integration2/take5/model/AI.java
@@ -1,5 +1,6 @@
 package be.kdg.integration2.take5.model;
-
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Arrays;
 
 public class AI extends Player{
@@ -7,5 +8,20 @@ public class AI extends Player{
     public void showHand(Deck deck){
         System.out.println(deck.aiHand.toString());
     }
+
+//    Rule, not done yet PLEASE don't delete it
+
+//    public void playLowestCardIfAvailable() {
+//        //  Sort the cards
+//        Collections.sort(getHand(), Comparator.comparing(Card::getValue));
+//
+//        //  Check the lowest card
+//        Card lowestCard = getHand().get(0);
+//        if (lowestCard.getValue() == 1) {
+//            // AI has the lowest card, play it first
+//            System.out.println("Playing lowest card: " + lowestCard.getValue());
+//            getHand().remove(lowestCard);
+//        }
+//    }
 
 }
